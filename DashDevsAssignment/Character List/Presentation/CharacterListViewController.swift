ec6da11e82b31802e5dc31d84758b0edd4282106
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-01 18:41:11 +0300

    Data source is set for `CharacterListViewController`

--- DashDevsAssignment/Character List/Presentation/CharacterListViewController.swift
+++ DashDevsAssignment/Character List/Presentation/CharacterListViewController.swift
@@ -6,7 +6,26 @@ import UIKit
 
 class CharacterListViewController: UIViewController {
 
+    private(set) var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CharacterListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
 }
