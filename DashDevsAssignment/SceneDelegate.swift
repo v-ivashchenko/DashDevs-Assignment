f7aa50c0bd33230183a7f77218979b6a9366d443
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-01 13:28:12 +0300

    Set `MARK`s in `SceneDelegate`

--- DashDevsAssignment/SceneDelegate.swift
+++ DashDevsAssignment/SceneDelegate.swift
@@ -6,11 +6,14 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
+    // MARK: - Private properties
     private lazy var navigationController = UINavigationController(rootViewController: rootViewController)
     private lazy var rootViewController = ViewController()
     
+    // MARK: - Public properties
     var window: UIWindow?
     
+    // MARK: - Public methods
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
