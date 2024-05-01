95468e873436a35e5ea8b5671c5d6ebb414993fd
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-01 20:22:27 +0300

    Introduce `anyURL` for testing purposes

--- DashDevsAssignmentTests/Shared/SharedTestHelpers.swift
+++ DashDevsAssignmentTests/Shared/SharedTestHelpers.swift
@@ -12,6 +12,10 @@ var anyData: Data {
     return Data("any data".utf8)
 }
 
+func anyURL() -> URL {
+    return URL(string: "https://any-url.com")!
+}
+
 func makeJSON(_ json: [String: Any]) -> Data {
     return try! JSONSerialization.data(withJSONObject: json)
 }
