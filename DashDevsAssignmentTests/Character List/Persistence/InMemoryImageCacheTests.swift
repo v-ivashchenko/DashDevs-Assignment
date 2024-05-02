29802488bc1d8ddd7b3c2e80cb81187bc857a2ec
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-02 23:21:34 +0300

    `InMemoryImageCache` returns nil on retrieving image from an empty cache

---
+++ DashDevsAssignmentTests/Character List/Persistence/InMemoryImageCacheTests.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © 2024 Vitalii Ivashchenko. All rights reserved.
+//
+
+import XCTest
+@testable import DashDevsAssignment
+
+final class InMemoryImageCacheTests: XCTestCase {
+
+    func test_image_returnsNilOnEmptyCache() async throws {
+        let key = UUID().uuidString
+        let sut = makeSUT()
+        
+        let resultImage = try await sut.image(by: key)
+        
+        XCTAssertNil(resultImage)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> ImageCache {
+        let sut = InMemoryImageCache()
+        
+        return sut
+    }
+}
