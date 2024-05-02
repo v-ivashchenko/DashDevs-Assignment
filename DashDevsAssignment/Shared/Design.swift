278f8c9332a51837c6fe5218fee5c70b9acd1c67
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-02 12:23:51 +0300

    Make `Design.Font` enum for custom fonts

---
+++ DashDevsAssignment/Shared/Design.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2024 Vitalii Ivashchenko. All rights reserved.
+//
+
+import UIKit
+
+enum Design {
+    
+    enum Font {
+        
+        static func latoBold(size: CGFloat = UIFont.labelFontSize) -> UIFont {
+            UIFont(name: "Lato-Bold", size: size)!
+        }
+        
+        static func latoRegular(size: CGFloat = UIFont.labelFontSize) -> UIFont {
+            UIFont(name: "Lato-Regular", size: size)!
+        }
+    }
+}
