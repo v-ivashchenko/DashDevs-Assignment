fc062c5baf706f7ab321164f725aed2e2d7c8df9
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-03 10:42:10 +0300

    Add `UINavigationController` extension to allow swipe back from pushed `UIHostingController`

---
+++ DashDevsAssignment/Shared/UINavigationController+gestureRecognizerShouldBegin.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2024 Vitalii Ivashchenko. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}
