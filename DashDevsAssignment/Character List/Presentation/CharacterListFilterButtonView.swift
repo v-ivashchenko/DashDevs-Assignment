d95d73eee3b78552cb2a422d32844ffb89500014
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2024-05-03 08:36:28 +0300

    Prototype `CharacterListFilterButtonView`

---
+++ DashDevsAssignment/Character List/Presentation/CharacterListFilterButtonView.swift
@@ -0,0 +1,34 @@
+//
+//  Copyright © 2024 Vitalii Ivashchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct CharacterListFilterButtonView: View {
+    
+    let text: String
+    let action: (() -> Void)?
+    
+    var body: some View {
+        Button {
+            action?()
+        } label: {
+            Text(text)
+                .font(Design.Font.latoBold().toFont)
+                .foregroundStyle(.accent)
+                .padding(10)
+                .overlay {
+                    Capsule()
+                        .stroke(.lightAccent3)
+                }
+        }
+    }
+}
+
+#Preview {
+    HStack {
+        CharacterListFilterButtonView(text: "Alive", action: nil)
+        CharacterListFilterButtonView(text: "Dead", action: nil)
+        CharacterListFilterButtonView(text: "Unknown", action: nil)
+    }
+}
